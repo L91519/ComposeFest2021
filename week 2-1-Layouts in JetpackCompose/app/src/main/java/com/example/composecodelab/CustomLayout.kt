338ce901eef468e411e4a78a90a138ccd99f42d5
Sumin Go <jennifer045020@gmail.com>
2021-11-17 10:50:05 +0900

    Staggered Layout 생성

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/composecodelab/CustomLayout.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/composecodelab/CustomLayout.kt
@@ -3,6 +3,7 @@ package com.example.composecodelab
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.Layout
+import kotlin.math.max
 
 @Composable
 fun CustomLayout(
@@ -12,10 +13,7 @@ fun CustomLayout(
     Layout(
         modifier = modifier,
         content = content
-    ) { measurables, constraints ->
-        val placeables = measurables.map { measurable ->
-            measurable.measure(constraints)
-        }
+    ) { _, constraints ->
         layout(constraints.maxWidth, constraints.maxHeight) {
             //place children view
         }
@@ -41,6 +39,42 @@ fun CustomColumn(
             placeables.forEach {
                 it.placeRelative(0, yPosition)
                 yPosition += it.height
+            }
+        }
+    }
+}
+
+@Composable
+fun StaggeredGrid(modifier: Modifier, rows: Int = 3, content: @Composable () -> Unit) {
+    Layout(content = content, modifier = modifier) { measureables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measureables.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints)
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = max(rowHeights[row], placeable.height)
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+        val height = rowHeights.sumOf { it }.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
             }
         }
     }
--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/composecodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/composecodelab/MainActivity.kt
@@ -18,8 +18,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.composecodelab.ui.BasicsCodelabTheme
@@ -56,11 +58,42 @@ fun LayoutsCodelab() {
     }
 }
 
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
 @Composable
 fun BodyContent(modifier: Modifier) {
-    Column {
-        LazyList()
-        PhotographerCard(modifier = modifier)
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        StaggeredGrid(modifier = modifier) {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp, 4.dp, 8.dp, 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
     }
 }
 
